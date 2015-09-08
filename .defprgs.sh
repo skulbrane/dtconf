5619744ae6c1420c1ba8b6d002a1d90a5e2b43cf
sektlab <sektlab@gmail.com>
2015-09-08 09:33:19 -0400

    add experimental script to install default programs, +tmux config, +zsh config

---
+++ .defprgs.sh
@@ -0,0 +1,17 @@
+# NodeJS
+###############################################################################
+
+# NodeJS setup script; adds doug lea ppa to sources most likely...
+curl -sL https://deb.nodesource.com/set_0.12 | sudo zsh -
+
+# install nodejs
+apt-get install -y nodejs
+
+# apps 
+###############################################################################
+apt-get install w3m
+apt-get install vim-nox
+
+
+
+
